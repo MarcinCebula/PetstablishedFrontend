e2460ce97a5fa0bd62afffe829ad27fef439842a
Marcin K Cebula <Marcin.K.Cebula@gmail.com>
2014-08-19 01:03:17 -0400

    Added Pagination to Pets and Shelters

--- app/scripts/pets-ctrl.js
+++ app/scripts/pets-ctrl.js
@@ -3,7 +3,10 @@
 angular.module('petstablished')
   .controller('PetsCtrl', ['$scope', '$http', '$state', '$stateParams', 'env', function ($scope, $http, $state, $params, env) {
     $scope.pagination = {
-      page: 1
+      page: 1,
+      limit: 20,
+      count: 0,
+      visible: false
     }
     $scope.shelter = {}
     $scope.pets = {}
@@ -25,15 +28,42 @@ angular.module('petstablished')
         active: true
       }]
     }
+
+    var calculatePagination = function() {
+      var offset = $scope.pagination.page * $scope.pagination.limit;
+      $scope.pagination.visible = (offset >= $scope.pagination.count) ? false : true;
+    }
+
+
+    $scope.loadMore = function() {
+      $scope.pagination.disable = true;
+      $scope.pagination.page += 1;
+      $http.get(env.api + '/shelter/' + $params.shelterUrlId +'/pets', {
+        params: {
+          page: $scope.pagination.page,
+          limit: $scope.pagination.limit
+        }
+      }).success(function(data, status, headers, config) {
+        $scope.pets = $scope.pets.concat(data.shelter_and_pets.pets);
+        $scope.pagination.count = data.shelter_and_pets.pet_count;
+        calculatePagination();
+      }).error(function(data, status, headers, config) {
+        alert(data.error)
+      });
+    };
+
     $http.get(env.api + '/shelter/' + $params.shelterUrlId +'/pets', {
       params: {
-        page: $scope.pagination.page
+        page: $scope.pagination.page,
+        limit: $scope.pagination.limit
       }
     }).success(function(data, status, headers, config) {
       $scope.shelter = data.shelter_and_pets.shelter;
       $scope.pets = data.shelter_and_pets.pets;
+      $scope.pagination.count = data.shelter_and_pets.pet_count;
+      calculatePagination();
       $scope.setupCrumbs();
     }).error(function(data, status, headers, config) {
-      // alert(data.error)
+      alert(data.error)
     });
   }]);
--- app/scripts/shelters-ctrl.js
+++ app/scripts/shelters-ctrl.js
@@ -3,7 +3,10 @@
 angular.module('petstablished')
   .controller('SheltersCtrl', ['$scope', '$http', '$state', '$stateParams', 'env', function ($scope, $http, $state, $params, env) {
     $scope.pagination = {
-      page: 1
+      page: 1,
+      limit: 10,
+      count: 0,
+      visible: false
     }
     $scope.setupCrumbs = function() {
       $scope.breadcrumbs = [{
@@ -18,14 +21,38 @@ angular.module('petstablished')
         active: true
       }]
     }
+
+    var calculatePagination = function() {
+      var offset = $scope.pagination.page * $scope.pagination.limit;
+      $scope.pagination.visible = (offset >= $scope.pagination.count) ? false : true;
+    }
+
+    $scope.loadMore = function() {
+      $scope.pagination.page += 1;
+      $http.get(env.api + '/shelter/index', {
+        params: {
+          page: $scope.pagination.page,
+          limit: $scope.pagination.limit
+        }
+      }).success(function(data, status, headers, config) {
+        $scope.shelters = $scope.shelters.concat(data.index_with_extras.shelters)
+        $scope.pagination.count = data.index_with_extras.count
+        calculatePagination();
+      }).error(function(data, status, headers, config) {
+        alert(data.error)
+      });
+    }
     $http.get(env.api + '/shelter/index', {
       params: {
-        page: $scope.pagination.page
+        page: $scope.pagination.page,
+        limit: $scope.pagination.limit
       }
     }).success(function(data, status, headers, config) {
-      $scope.shelters = data
+      $scope.shelters = data.index_with_extras.shelters
+      $scope.pagination.count = data.index_with_extras.count
+      calculatePagination();
       $scope.setupCrumbs();
     }).error(function(data, status, headers, config) {
-      // alert(data.error)
+      alert(data.error)
     });
   }]);
